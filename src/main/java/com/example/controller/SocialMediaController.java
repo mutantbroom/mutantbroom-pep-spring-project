b0c81ffbdf0f8b3a3049257580b21449d96522ca
Jonathan Beck <jon.beck@live.com>
2025-07-11 21:29:04 +0000

    Updated social media controller

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -9,4 +9,76 @@ package com.example.controller;
  */
 public class SocialMediaController {
 
+    private final AccountService accountService;
+    private final MessageService messageService;
+
+    public SocialMediaController(AccountService accountService, MessageService messageService) {
+        this.accountService = accountService;
+        this.messageService = messageService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Account> registerAccount(@RequestBody Account potentialNewAccount) {
+        Account returnedAccount = accountService.register(potentialNewAccount);
+        return ResponseEntity.ok(returnedAccount);
+    }
+
+    @PostMapping("login")
+    public ResponseEntity<Account> loginAccount(@RequestBody Account potentialLogin) {
+        Account returnedAccount = accountService.login(potentialLogin);
+        return ResponseEntity.ok(returnedAccount);
+    }
+
+
+
+    
+    @PostMapping("messages")
+    public ResponseEntity<Message> postMessage(@RequestBody Message potentialMessage) {
+        Message returnedMessage = messageService.post(potentialMessage);
+        return ResponseEntity.ok(returnedMessage);
+    }
+
+
+
+    
+    @GetMapping("messages")
+    public ResponseEntity<List<Message>> getMessages() {
+        List<Message> returnedMessages = messageService.getAll();
+        return ResponseEntity.ok(returnedMessages);
+    }
+
+
+
+    
+    @GetMapping("messages/{messageId}")
+    public ResponseEntity<Message> getMessage(@PathVariable int messageId) {
+        Message returnedMessage = messageService.getMessage(messageId);
+        return ResponseEntity.ok(returnedMessage);
+    }
+
+
+    
+    @DeleteMapping("messages/{messageId}")
+    public ResponseEntity<Integer> deleteMessage(@PathVariable int messageId) {
+        int affectedRows = messageService.delete(messageId);
+        return (affectedRows == 0) ? ResponseEntity.ok().build() : ResponseEntity.ok(affectedRows);
+    }
+
+
+    
+    @PatchMapping("messages/{messageId}")
+    public ResponseEntity<Integer> patchMessage(@PathVariable int messageId, @RequestBody Message potentialPatchedMessage) {
+        int affectedRows = messageService.patch(messageId, potentialPatchedMessage);
+        return ResponseEntity.ok(affectedRows);
+    }
+
+
+
+    
+    @GetMapping("accounts/{accountId}/messages")
+    public ResponseEntity<List<Message>> getMessagesForUser(@PathVariable int accountId) {
+        List<Message> returnedMessages = messageService.getByAccountId(accountId);
+        return ResponseEntity.ok(returnedMessages);
+    }
+
 }
