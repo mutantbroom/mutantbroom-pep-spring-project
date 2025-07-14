6081c930d463021de8f3729fb730fdc6b24f31d0
Jonathan Beck <jon.beck@live.com>
2025-07-14 18:15:23 +0000

    updated and fixed errors with account/message repository files

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -1,15 +1,17 @@
 package com.example.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import com.example.entity.Message;
+
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface MessageRepository {
+public interface MessageRepository extends JpaRepository<Message, Integer> {
 
     Optional<Message> findMessageByMessageId(int messageId);
 
