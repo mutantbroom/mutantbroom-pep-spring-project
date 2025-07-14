a8cdf93e1ec53e15fd487f83615908835ba0284d
Jonathan Beck <jon.beck@live.com>
2025-07-14 18:34:58 +0000

    more login

--- src/main/java/com/example/exception/LoginException.java
+++ src/main/java/com/example/exception/LoginException.java
@@ -3,7 +3,7 @@ package com.example.exception;
 public class LoginException extends RuntimeException {
     
     public LoginException(String message) {
-        super("Login error: " + message);
+        super(message);
     }
     
 }
